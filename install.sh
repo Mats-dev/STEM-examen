76267dbc0f6cfa28e59ec1d477479da0e857aa02
Mats-dev <134952466+Mats-dev@users.noreply.github.com>
2024-06-07 18:28:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 sudo apt-get update
 sudo apt-get upgrade
-
-sudo apt-get install python3-picamera python3-rpi.gpio python3-opencv
-pip3 install tensorflow keras
+sudo apt-get install python3-picamera python3-rpi.gpio
