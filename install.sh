b76c698bf3fafeb0f31d639f1d25f3b9494cde90
Mats-dev <134952466+Mats-dev@users.noreply.github.com>
2024-06-07 19:44:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install python3-picamera python3-rpi.gpio
+sudo apt-get install python3-pip
+pip3 install picamera RPi.GPIO opencv-python numpy
+pip3 install Adafruit_PN532
